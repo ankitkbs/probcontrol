e6985842cbf519fa37c3f5ff4b20880d4c8b8202
ankitcorejava <107574427+ankitcorejava@users.noreply.github.com>
2025-05-14 01:15:12 +0530

    docs: add understanding document Initial commit: Setup Spring Boot project with dependencies

---
+++ src/main/java/com/example/demo/ProbcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbcontrolApplication.class, args);
+	}
+
+}
